136a4ce04d808758da9874bb6b2c43efb75f7bfa
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-10-25 15:12:41 +0530

    🧚‍♂️ ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ 🧚‍♂️

--- commands/yt-playlist.js
+++ commands/yt-playlist.js
@@ -64,8 +64,8 @@ yts( opts, async function ( err, playlist ) {
                                 thumbnail: log0,
                                 renderLargerThumbnail: true,
                                 mediaType: 2,
-                                mediaUrl: 'https://github.com/SamPandey001/Secktor-Md',
-                                sourceUrl: 'https://github.com/SamPandey001/Secktor-Md'
+                                mediaUrl: 'https://github.com/Sithuwa/SITHUWA-MD',
+                                sourceUrl: 'https://github.com/Sithuwa/SITHUWA-MD'
                             }
                         }
                     }
