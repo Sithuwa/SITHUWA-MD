16de4f855aa3f9e9f3a7a634da97aadc74fd1a04
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-10-25 15:05:05 +0530

    🧚‍♂️ ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ 🧚‍♂️

--- commands/reactionpack.js
+++ commands/reactionpack.js
@@ -1,10 +1,10 @@
 /**
- Copyright (C) 2022.
+ Copyright (C) 2023.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
+ * @project_name : Sithum-Md
+ * @author : SithumKalhara <https://github.com/Sithuwa>
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
  
