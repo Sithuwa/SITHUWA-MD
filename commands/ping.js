4f3ba825421ceacd3823384c7a270c13d85a82d1
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-10-25 15:00:25 +0530

    🧚‍♂️ ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ 🧚‍♂️

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 /**
- Copyright (C) 2022.
+ Copyright (C) 2023.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : Sithuwa-Md
+ * @author : Sithum Kalhara <https://github.com/Sithuwa>
+ * @description : Sithuwa,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -21,7 +21,7 @@ Secktor.cmd({
         var inital = new Date().getTime();
         const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
         var final = new Date().getTime();
-       // await Secktor.sleep(1000)
+       // await Sithuwa.sleep(1000)
        return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );
