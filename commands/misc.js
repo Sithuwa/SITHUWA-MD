4bf7ab7bdcf62b98373c5d199c30ed1744f75dd9
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-10-26 19:25:27 +0530

    🧚‍♂️ ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ 🧚‍♂️

--- commands/misc.js
+++ commands/misc.js
@@ -821,7 +821,7 @@ async(Void, citel, text,{isCreator}) => {
 })   
 } // if Statements
      //---------------------------------------------------------------------------
- 
+     
  cmd({
              pattern: "antispam",
              desc: "Kick Spamers From Group.\nuse buttons to toggle.",
@@ -855,82 +855,6 @@ if (checkgroup.antispam == "true") return citel.reply(`Antispam : kick Users Who
 else return citel.reply(`Antispam : kick Users Who Spamming in Groupn\n\nAntispam is Disabled in this Group \n *_For Enablling Antispam : ${prefix}antispam on_*`);
          
  })
- 
-     //---------------------------------------------------------------------------
-     cmd({
-		pattern: 'antilink ?(.*)',
-		fromMe: true,
-		desc: 'to on off antiLink',
-		type: 'group',
-		onlyGroup: true,
-	},
-	async (message, match) => {
-		const antilink = await getAntiLink(message.jid)
-		if (!match) {
-			const onOrOff = antilink.enabled ? 'on' : 'off'
-			return await message.send(
-				`_Antilink is ${onOrOff}_\n*Example :*\nantilink list\nantilink whatsapp.com\nantlink on | off`
-			)
-			// const button = await genButtonMessage(
-			// 	[
-			// 		{ id: 'antilink info', text: 'INFO' },
-			// 		{ id: `antilink ${onOrOff}`, text: onOrOff.toUpperCase() },
-			// 	],
-			// 	'Example\nhttps://github.com/lyfe00011/whatsapp-bot-md/wiki/antilink',
-			// 	'Antilink'
-			// )
-			// return await message.send(button, {}, 'button')
-			// return await message.send(
-			// 	await genHydratedButtons(
-			// 		[
-			// 			{
-			// 				urlButton: {
-			// 					text: 'Example',
-			// 					url: 'https://github.com/lyfe00011/whatsapp-bot-md/wiki/antilink',
-			// 				},
-			// 			},
-			// 			{
-			// 				button: {
-			// 					id: `antilink ${antilink.enabled ? 'off' : 'on'}`,
-			// 					text: antilink.enabled ? 'OFF' : 'ON',
-			// 				},
-			// 			},
-			// 			{ button: { id: 'antilink info', text: 'INFO' } },
-			// 		],
-			// 		'AntiLink'
-			// 	),
-			// 	{},
-			// 	'template'
-			// )
-		}
-		if (match == 'on' || match == 'off') {
-			if (match == 'off' && !antilink)
-				return await message.send('_AntiLink is not enabled._')
-			await setAntiLink(message.jid, match == 'on')
-			return await message.send(
-				`_AntiLink ${match == 'on' ? 'Enabled' : 'Disabled.'}_`
-			)
-		}
-		if (match == 'info')
-			return await message.send(
-				`*AntiLink :* ${antilink.enabled ? 'on' : 'off'}\n*AllowedUrl :* ${
-					antilink.allowedUrls
-				}\n*Action :* ${antilink.action}`
-			)
-		if (match.startsWith('action/')) {
-			await setAntiLink(message.jid, match)
-			const action = match.replace('action/', '')
-			if (!['warn', 'kick', 'null'].includes(action))
-				return await message.send('*Invalid action*')
-			return await message.send(`_AntiLink action updated as ${action}_`)
-		}
-		const res = await setAntiLink(message.jid, match)
-		return await message.send(
-			`_AntiLink allowed urls are_\nAllow - ${res.allow.join(
-				', '
-			)}\nNotAllow - ${res.notallow.join(', ')}`
-		)
-	})
      //---------------------------------------------------------------------------
  cmd({ on: "body" }, async(Void, citel) => {
    if (!Config.autoreaction) return 
