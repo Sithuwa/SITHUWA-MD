607369509c7095b9ed36a6d57a8b34255e5d6bf0
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-01 21:14:40 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -680,8 +680,9 @@ cmd({
             let infoYt = await ytdl.getInfo(urlYt);
             if (infoYt.videoDetails.lengthSeconds >= 1200) return citel.reply(`*song not Found, Try Differ Name*`);
             let titleYt = infoYt.videoDetails.title;   
-	    citel.reply(`*Downloading..*`);
-	   citel.reply(`ᴍᴏᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ 💛`);
+	    message.reply(`${text[LANGUAGE].FOUNDED} "${title}"`);
+
+            message.reply(text[LANGUAGE].DOWNLOAD_STARTED);
             let randomName = getRandom(".mp3");
             const stream = ytdl(urlYt, {
                  filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128, })
