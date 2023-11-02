c627558674aae6e13500b7b94f376c264772dc9c
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-02 18:33:01 +0530

    Update VoiceReply.js

--- commands/VoiceReply.js
+++ commands/VoiceReply.js
@@ -1,1 +1,17 @@
 
+client.on('message', (message) => {
+  // Send a voice reply to every message
+  client.sendMessage(message.from, {
+    audio: {
+      url: 'https://example.com/audio.mp3',
+    },
+  });
+});
+
+// Schedule a task to send the voice reply every minute
+new Cron.CronJob('0 * * * *', () => {
+  // Send the voice reply
+}, null, true);
+
+client.connect();
+
