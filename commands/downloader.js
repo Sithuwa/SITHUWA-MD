5d7ee9f1cca0257e27122704f9510780a179419f
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-02 09:53:20 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -666,13 +666,8 @@ cmd({
                 let yts = require("secktor-pack")
                 let search = await yts(text);
                 let i = search.all[1] ;
-                let cap = "\t *---Yt Song Searched Data---*   \n\n*Title :* " + i.title + "\n*Url :* " + i.url +"\n*Description :* " + i.timestamp +"\n*Views :* "+i.views +"\n*Uploaded :* " +i.ago +"\n*Author :* "+i.author.name+"\n\n\nReply 1 To Video \nReply 2 To Audio" ;
+                let cap = "\t *---Yt Song Searched Data---*   \n\nTitle : " + i.title + "\nUrl : " + i.url +"\nDescription : " + i.timestamp +"\nViews : "+i.views +"\nUploaded : " +i.ago +"\nAuthor : "+i.author.name+"\n\n\nReply 1 To Video \nReply 2 To Audio" ;
                 Void.sendMessage(citel.chat,{image :{url : i.thumbnail}, caption :  cap });
-           
-           
-           
-           
-           
            
             
            
