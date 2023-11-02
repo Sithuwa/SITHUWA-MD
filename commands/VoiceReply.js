a8f8c6173d668802f0cf955986a16edddb6f7dab
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-02 22:35:28 +0530

    Update VoiceReply.js

--- commands/VoiceReply.js
+++ commands/VoiceReply.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = '.VoiceClip/Adarei.mp3'
+let vn = './VoiceClip/Adarei.mp3'
 conn.sendFile(m.chat, vn, 'Adarei.mp3', null, m, true, {
 type: 'audioMessage', 
-ptt: true
+ptt: true 
 })
 }
-handler.customPrefix = /VoiceReply|VoiceReply/ 
+handler.customPrefix = /Adarei|adarei|ADAREI/ 
 handler.command = new RegExp
 module.exports = handler
