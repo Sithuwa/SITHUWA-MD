43c1bd738b838dfe9aa786c0eae89a7096793589
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-03 00:12:23 +0530

    Update flash_mp3.js

--- commands/flash_mp3.js
+++ commands/flash_mp3.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/flash.mp3'
-conn.sendFile(m.chat, vn, 'flash.mp3', null, m, true, {
+let vn = './media/bot.mp3'
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Me voy|me voy|ME VOY|Me fui|me fui|ME FUI|Se fue|se fue|SE FUE|Adios|adios|ADIOS|Chao|chao|CHAO/i 
+handler.customPrefix = /bot|me voy|Bot|Me fui|me fui|ME FUI|Se fue|se fue|SE FUE|Adios|adios|ADIOS|Chao|chao|CHAO/i 
 handler.command = new RegExp
 module.exports = handler
