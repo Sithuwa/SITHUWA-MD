7a48fb9e7db4cd95af04a1d7f0dbe44abb32ff53
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-03 10:32:32 +0530

    Create Hola.js

---
+++ commands/Hola.js
@@ -0,0 +1,15 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/bot.mp3'
+conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
+}
+handler.customPrefix = /bot|Bot|enojado|Molesto|Enojada|ENOJADA|enojada|Molesta/i 
+handler.command = new RegExp
+handler.fail = null
+handler.exp = 100
+module.exports = handler
