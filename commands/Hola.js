a94d3a28cd351eec442da359f938af280f50b37b
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-03 10:49:08 +0530

    Update Hola.js

--- commands/Hola.js
+++ commands/Hola.js
@@ -2,7 +2,7 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = '/media/bot.mp3'
+let vn = '../media/bot.mp3'
 conn.sendFile(m.chat, vn, 'bot.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
