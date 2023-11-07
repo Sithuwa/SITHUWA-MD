46432bbfe28cf5304cc3a89bd0b49b2e6cb5a122
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-07 10:14:00 +0530

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -337,9 +337,9 @@ cmd({
             filename: __filename,
             desc: "is bot alive??"
         },
-        await Void.sendPresenceUpdate('recording', citel.chat);
-        await Void.sendMessage(citel.chat, { audio: {url : 'https://github.com/nipuna15/Voice/raw/main/Alive.mp3',}, mimetype: 'audio/mpeg', ptt: true }, { quoted: citel, });
         async(Void, citel, text, isAdmins) => {
+	    await Void.sendPresenceUpdate('recording', citel.chat);
+            await Void.sendMessage(citel.chat, { audio: {url : 'https://github.com/nipuna15/Voice/raw/main/Alive.mp3',}, mimetype: 'audio/mpeg', ptt: true }, { quoted: citel, });
             let alivemessage = Config.ALIVE_MESSAGE || `*ᴀ ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ.*`
             const alivtxt = `
 *Hello, ${citel.pushName},*
