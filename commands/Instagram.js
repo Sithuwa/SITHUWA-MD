992987adf1ed838744f4e563e5f7b74653ebc979
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-08 16:26:44 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,51 +1,41 @@
 const {cmd } = require('../lib')
+
 cmd({
-        pattern: "insta",
-        desc: "download instagram post.",
-        category: "downloader",
-        filename: __filename
-    },
-    async(core) => {
-	var instavid = /(?:\/p\/|\/reel\/|\/tv\/)([^\s&]+)/, instastory = /\/stories\/([^\s&]+)/
-	var { url, type } = core.input && instavid.test(core.input) ? { url:core.input, type:'vid' } : core.input && instastory.test(core.input) ? { url:core.input, type:'story' } : { url:false, type:false }
-	if(!url) {
-		return await core.reply(string().insta.need)
-	}
+            pattern: "insta",
+            desc: "Downloads video from insta.",
+            category: "downloader",
+            filename: __filename,
+            use: '<faded-Alan Walker>',
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply(`Example : ${prefix}video Back in black`)
+            let instadl = require("secktor-pack")
+            let search = await instadl(text)
+	    let i = search.all[1] ;
+	    	
+            const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`;   };
+            try {
+		let urlinsta = i.url ; 
+                let infoinsta = await instadl.getInfo(urlinsta);
 
-	if(type == 'story') {
-		return await core.reply(string().insta.story)
-	}
+                let VidTime = Math.floor(i.timestamp* 60);
+		if( VidTime  >= videotime) return await citel.reply(`❌ Video file too big!`);
+                let titleinsta = infoinsta.videoDetails.title;
+                let randomName = getRandom(".mp4");
+                const stream = instadl(urlinsta, {   filter: (info) => info.itag == 22 || info.itag == 18, })
+                    .pipe(fs.createWriteStream(`./${randomName}`));
+                await new Promise((resolve, reject) => {
+                    stream.on("error", reject);
+                    stream.on("finish", resolve);
+                });
+                   
+		let buttonMessage = {
+                        video: fs.readFileSync(`./${randomName}`),
+                        mimetype: 'video/mp4',
+                        caption: "  •ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ• " + Config.caption ,
+                    }
+                 Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
+                 return fs.unlinkSync(`./${randomName}`);
 
-	try {
-		await core.send((dataDb.InstaDownload || string().insta.dload).setup(core))
-		var data = await axios({
-			method: 'GET',
-			url : Api_url,
-			params : {
-				api:process.env.Ravindu_Manoj_Api,
-				code: 'instagram',
-				url
-			}
-		})
-		if(data.data.code != 200) {
-			return await core.reply('Invalid Request' + data.data.error)
-		}
-
-		await core.send((dataDb.InstaUplaod || string().insta.uload).setup(core))
-
-		var linkdata = await linkPreview(core.input)
-		if(!linkdata.mime || linkdata.mime === 'text/html') {
-			return await core.reply('*I Am Not Expert For Download This Link*')
-		}
-
-		if(linkdata.mime.cut('/')[0] == 'image') {
-			return await core.mediasend('image', data.data.result.url, dataDb.caption.setup(core))
-		}
-
-		if(linkdata.mime.cut('/')[0] == 'video') {
-			return await core.mediasend('video', data.data.result.url, dataDb.caption.setup(core))
-		}
-	} catch(e) {
-		await core.reply(string().insta.error)
-	}
-}
+                }catch(e){return await citel.reply("Error While Downloading Video : " + e ); }
+		    
