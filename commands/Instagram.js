ccafc417bdfd3cd66df9bc57e03e62674567885d
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-08 18:21:52 +0530

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -10,7 +10,7 @@ cmd({
 if(!text) return citel.reply('Need post url.')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+await Void.sendFileUrl(citel.chat, response[i], (`*Downloaded Media from instagram.*`)
         
 Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
 return fs.unlinkSync(`./${randomName}`);
