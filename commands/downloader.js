525893da14cde219132c3879e5cf4b5ece289183
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-10 13:39:04 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -193,26 +193,27 @@ let buttonMessage =
 
 })
 //----------------------------------------------------------------------------------
- cmd(
+
+cmd(
   {
     pattern: "fb ?(.*)",
     fromMe: true,
     desc: Lang.FB_DESC,
   },
-  async (message, match) => {
-    match = !message.reply_message ? match : message.reply_message.text
-    if (match === "") return await message.sendMessage(Lang.NEED_REPLY)
-    await message.sendMessage(Lang.DOWNLOADING)
+  async (citel, match) => {
+    match = !citel.reply_message ? match : citel.reply_message.text
+    if (match === "") return await citel.sendMessage(Lang.NEED_REPLY)
+    await citel.sendMessage(Lang.DOWNLOADING)
     let links = await downVideo(match)
-    if (links.length == 0) return await message.sendMessage(Lang.NOT_FOUND)
+    if (links.length == 0) return await citel.sendMessage(Lang.NOT_FOUND)
     let { buffer, size } = await getBuffer(links[0])
     if (size > 100)
-      return await message.sendMessage(
+      return await citel.sendMessage(
         Lang.SIZE.format(size, links[0], links[1])
       )
-    return await message.sendMessage(
+    return await citel.sendMessage(
       buffer,
-      { quoted: message.quoted, caption: Lang.CAPTION.format(links[1] || "") },
+      { quoted: citel.quoted, caption: Lang.CAPTION.format(links[1] || "") },
       MessageType.video
     )
   }
