5c5cde229f8c73e3e3c629630456ae9d438afe38
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-11-26 22:01:18 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,11 +1,11 @@
 /**
- Copyright (C) 2023.
+ Copyright (C) 2022.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : SITHUWA-MD
+ * @project_name : XLICON-MD
  * @author : SalmanYtOfficial <https://github.com/salmanytofficial>
- * @description : SITHUWA ,A Multi-functional whatsapp bot.
+ * @description : XLICON ,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -19,7 +19,6 @@ const fetch = require('node-fetch')
 cmd({
             pattern: "imdb",
             category: "search",
-	    react :'🖼️',
             desc: "Sends image of asked Movie/Series.",
             use: '<text>',
             filename: __filename,
@@ -62,7 +61,6 @@ cmd({
 cmd({
             pattern: "weather",
             category: "search",
-	    react :'🗯',
             desc: "Sends weather info about asked place.",
             use: '<location>',
             filename: __filename,
@@ -93,7 +91,6 @@ cmd({
 cmd({
             pattern: "horo",
             category: "search",
-	    react :'👻',
             desc: "Gives horoscope info of user.",
             use: '<sign>\n:Example: horo libra',
             filename: __filename,
@@ -132,13 +129,12 @@ cmd({
         pattern: "google",
         alias :['search','gsearch'],
         category: "search",
-	react :'✅',
         desc: "Sends info of given query from Google Search.",
         use: '<text>',
         filename: __filename,
     },
     async(Void, citel, text) => {
-        if (!text) return citel.reply(`give me a query\n*Example : .google Who is Sithuwa Md.*`);
+        if (!text) return citel.reply(`give me a query\n*Example : .google Who is Suhail Tech.*`);
         let google = require('google-it');
         google({ 'query': text}).then(res => {
             let msg= `Google Search From : ${text} \n\n`;
@@ -152,27 +148,10 @@ cmd({
         })
     }
 )
-  //---------------------------------------------------------------------------
-    cmd({
-        pattern: "truecall",
-        alias :['trucall','tcall'],
-        category: "search",
-	react :'📞',
-        desc: "Sends info of given query from truecall Search.",
-        use: '<text>',
-        filename: __filename,
-    },
-   async (Void, citel, text) => {
-	if (!text && !citel.quoted) return await citel.reply('_Enter the number you want to search_');
-	const number = citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, '')
-	const result = await truecaller(number, citel.user_id)
-	return await citel.reply(result);
-})
     //---------------------------------------------------------------------------
 cmd({
             pattern: "image",
             category: "search",
-	    react :'🖼️',
             desc: "Searches Image on Google",
             use: '<text>',
             filename: __filename,
@@ -188,33 +167,14 @@ cmd({
 
                 let n = await gis(name1)
                     images = n[Math.floor(Math.random() * n.length)].url;
-                    await Void.sendMessage(citel.chat, {image: {  url: images,}, caption: `_SITHUWA Image Search_\n*${name1}*`,}, { quoted: citel, });
+                    await Void.sendMessage(citel.chat, {image: {  url: images,}, caption: `_Xlicon Image Search_\n*${name1}*`,}, { quoted: citel, });
             }
         }
     )
     //---------------------------------------------------------------------------
-
-cmd({
-        pattern: "cricket",
-        alias :['cs','mach'],
-        category: "search",
-	react :'💣',
-        desc: "Cricket Search.",
-        use: ' ',
-        filename: __filename,
-    },
-    async(bot, man, tax) => {
-
-          man.send (`*_Please Wait, While Getting Cricket Updates_*`);
-const _0x485f13=_0xc8fc;(function(_0xea9292,_0x3f139b){const _0x213a89=_0xc8fc,_0x3e0c50=_0xea9292();while(!![]){try{const _0x36daa8=-parseInt(_0x213a89(0x78))/(0x3b3+-0x23da+-0x405*-0x8)+parseInt(_0x213a89(0x6c))/(-0x915+-0x3c4*0x8+0x2737*0x1)+parseInt(_0x213a89(0x70))/(0x1a8c+-0xdf3*-0x1+-0x287c)+parseInt(_0x213a89(0x82))/(0xe17+0x1*-0x12f+-0xce4)*(-parseInt(_0x213a89(0x76))/(-0x6ec+0x21bd+-0x1acc))+-parseInt(_0x213a89(0x81))/(0x1*0x169f+-0x26e4+0x2b*0x61)+-parseInt(_0x213a89(0x87))/(0xdc2+0xf6b+-0x1*0x1d26)+parseInt(_0x213a89(0x8a))/(0x8*-0x353+-0x5aa+0x204a);if(_0x36daa8===_0x3f139b)break;else _0x3e0c50['push'](_0x3e0c50['shift']());}catch(_0x27ba26){_0x3e0c50['push'](_0x3e0c50['shift']());}}}(_0x33fc,0x3e36*-0xa+-0x154cdc+0xe*0x287c5));const response=await fetch(_0x485f13(0x86)+_0x485f13(0x7d)+_0x485f13(0x6f)+_0x485f13(0x73)+_0x485f13(0x79)+_0x485f13(0x7b)+_0x485f13(0x77)+_0x485f13(0x8d)+_0x485f13(0x7f)),dat=await response[_0x485f13(0x88)]();function _0xc8fc(_0x2f821a,_0x33dadc){const _0x5a8907=_0x33fc();return _0xc8fc=function(_0x19d3dd,_0xd34c1d){_0x19d3dd=_0x19d3dd-(-0x471*-0x4+0x17f5+-0x2952);let _0x2eab78=_0x5a8907[_0x19d3dd];return _0x2eab78;},_0xc8fc(_0x2f821a,_0x33dadc);}function _0x33fc(){const _0x4576c5=['reply','\x0a*•𝙼𝙰𝚃𝙲𝙷\x20𝙴','com/v1/cur','3854604oDkQWW','𝚃𝙰𝚃𝚄𝚂•*\x20','𝙰𝚃𝙴•*\x20','rentMatche','length','matchStart','2210285SqEbaS','c9-47c5-8f','1300428xSrjMf','s?apikey=f','𝙰𝙼𝙴•*\x20','68d1cb5-a9','\x0a*✯────𝐌𝐀𝐓','i.cricapi.','𝙽𝙳𝙴𝙳•*\x20','ace78','\x0a*•𝙼𝙰𝚃𝙲𝙷\x20𝙽','1610550BQOseR','4lHQhEe','𝚃𝙰𝚁𝚃𝙴𝙳•*\x20','matchEnded','data','https://ap','4494392EmBlJn','json','\x0a*•𝙼𝙰𝚃𝙲𝙷\x20𝙳','12533368EWYRzv','name','status','cd-fbfe52b','dateTimeGM','log','𝐂𝐇\x20','────✯*','\x0a*•𝙼𝙰𝚃𝙲𝙷\x20𝚂','1135682ImKxKM'];_0x33fc=function(){return _0x4576c5;};return _0x33fc();}console[_0x485f13(0x68)](dat);for(let i=-0x6b*0x2+0x90d*-0x2+0x12f0;i<dat[_0x485f13(0x85)][_0x485f13(0x74)];i++){let j=i+(-0x12*-0x10b+0x5d*0x68+-0x388d);tax+=_0x485f13(0x7c)+_0x485f13(0x69)+i+_0x485f13(0x6a),tax+=_0x485f13(0x80)+_0x485f13(0x7a)+dat[_0x485f13(0x85)][i][_0x485f13(0x8b)],tax+=_0x485f13(0x6b)+_0x485f13(0x71)+dat[_0x485f13(0x85)][i][_0x485f13(0x8c)],tax+=_0x485f13(0x89)+_0x485f13(0x72)+dat[_0x485f13(0x85)][i][_0x485f13(0x67)+'T'],tax+=_0x485f13(0x6b)+_0x485f13(0x83)+dat[_0x485f13(0x85)][i][_0x485f13(0x75)+'ed'],tax+=_0x485f13(0x6e)+_0x485f13(0x7e)+dat[_0x485f13(0x85)][i][_0x485f13(0x84)]+'\x0a';}return await man[_0x485f13(0x6d)](tax);
-
-
-})
-//-----------------------------------------------------------------------------
 cmd({
             pattern: "couplepp",
             category: "search",
-	    react :'👩‍❤️‍👨',
             desc: "Sends two couples pics.",
             filename: __filename,
         },
@@ -229,14 +189,13 @@ cmd({
 cmd({
         pattern: "iswa",
         category: "search",
-	react :'🪀',
         desc: "Searches in given rage about given number.",
-        use: '94xxxxxxxxx',
+        use: '9112345678xx',
         filename: __filename,
     },
     async(Void, citel, text) => {
         var inputnumber = text.split(" ")[0]
-        if (!inputnumber.includes('x')) return citel.reply('You did not add x\nExample: iswa 94xxxxxxxxx')
+        if (!inputnumber.includes('x')) return citel.reply('You did not add x\nExample: iswa 9196285162xx')
         citel.reply(`Searching for WhatsApp account in given range...`)
 
         function countInstances(string, word) {
