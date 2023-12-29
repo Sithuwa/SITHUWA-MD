031774cc5229da20e8d34c82f9a5c1f8dfea716f
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-12-29 23:54:56 +0530

    🧚‍♂️ ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ 🧚‍♂️

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -11,15 +11,15 @@ cmd({
     pattern: 'lyrics',
     desc: lang.LYRICS.DESC,
     type: "search"
-}, async (m,match) => {
-    match = match || m.reply_message.text;
-    if(!match) return await m.send(lang.BASE.TEXT);
-    const res = await fetchJson(config.BASE_URL+'api/lyrics?text='+match);
-    if(!res.status) return m.send(lang.BASE.ERROR.format("Not Found"));
-    if(!res.result) return m.send(lang.BASE.ERROR.format(",try again"));
+}, async (Void,citel,text) => {
+    text = text || citel.reply_message.text;
+    if(!text) return await citel.send(lang.BASE.TEXT);
+    const res = await fetchJson(config.BASE_URL+'api/lyrics?text='+text);
+    if(!res.status) return citel.send(lang.BASE.ERROR.format("Not Found"));
+    if(!res.result) return citel.send(lang.BASE.ERROR.format(",try again"));
     const { thumb,lyrics,title,artist } = res.result, tbl= "```", tcl ="*_", tdl = "_*";
         const msg = lang.LYRICS.RESPONCE.format(tcl+artist+tdl,tcl+title+tdl)+`\n\n${tbl}${lyrics}${tbl}`;
-        return await m.client.sendMessage(m.from, {
+        return await citel.client.sendMessage(citel.from, {
             image: {url : thumb},
             caption :msg
         }, {
