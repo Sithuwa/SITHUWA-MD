2c660faa381c02af3140dcaf214bc58035d981a7
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2023-12-29 20:20:21 +0530

    🧚‍♂️ ꜱɪᴛʜᴜᴡᴀ-ᴍᴅ 🧚‍♂️

--- commands/misc.js
+++ commands/misc.js
@@ -882,3 +882,54 @@ else return citel.reply(`Antispam : kick Users Who Spamming in Groupn\n\nAntispa
      }
  
  })
+
+
+//-----------------------------------------------------
+
+cmd({
+        pattern: "find",
+        category: "search",
+        react: "🎶",
+        desc: "Finds info about song"
+    },
+    async(Void, citel, args) => {
+        let mime = citel.quoted.mtype
+        if (!citel.quoted) return citel.reply(`Send/Reply Image ${prefix}shaxam`);
+        if (!/audio/.test(mime)) return citel.reply(`Send/Reply audio ${prefix}shazam`);
+        let buff = await citel.quoted.download();
+        let data = await shazam(buff);
+        if (!data.status) return citel.reply(data);
+	let search = await yts(data.title);
+let anu = search.videos[0];
+          let h =  `*Title :* _${data.title}_           
+*Artist :* _${data.artists}_            
+*Album :* _${data.album}_                   
+*Release :* _${data.release_date}
+
+*To Download Song*
+${prefix}ytmp3 ${anu.url}
+`
+
+let buttonMessaged = {
+				image: {
+                                      url: anu.thumbnail,
+                                       },
+				caption: h,
+				footer: tlang().footer,
+				headerType: 4,
+				contextInfo: {
+					externalAdReply: {
+						title: data.artists,
+						body: data.album,
+						thumbnail: log0,
+						mediaType: 2,
+						mediaUrl: ``,
+						sourceUrl: ``,
+					},
+				},
+			};
+			await Void.sendMessage(citel.chat, buttonMessaged, {
+				quoted: citel,
+			});
+    }
+ )
